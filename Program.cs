0b18cc1f7f544239de446c287e354a336e069d8e
swapna <reddyswapnak@gmail.com>
2022-06-01 13:30:37 +1200

    second commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Navigate().GoToUrl("http://www.google.com");
+driver.FindElement(By.Name("q")).SendKeys("learn Automation");
